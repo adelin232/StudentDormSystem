41d88610cbe761cdbc99624afbb499815d08f50e
adelin232 <narcis.adelin.miulet@gmail.com>
2022-11-26 16:10:23 +0200

    Define get method on Service layer using CompletableFuture

--- src/main/java/studentsdorm/platform/Student/StudentController.java
+++ src/main/java/studentsdorm/platform/Student/StudentController.java
@@ -1,7 +1,24 @@
 package studentsdorm.platform.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students/{id}")
+    @ResponseBody
+    public String getStudent(@PathVariable final Long id) {
+
+//        studentService.getStudent(id);
+
+        return id.toString();
+    }
 }
--- src/main/java/studentsdorm/platform/Student/StudentService.java
+++ src/main/java/studentsdorm/platform/Student/StudentService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class StudentService {
@@ -12,14 +14,22 @@ public class StudentService {
     @Autowired
     private StudentRepo studentRepo;
 
-    public StudentEntity getStudent(final Long id) {
+    public StudentEntity getStudent(final Long id) throws ExecutionException, InterruptedException {
 
-        List<StudentEntity> students = studentRepo.findAll().stream().filter(studentEntity -> Objects.equals(studentEntity.getId(), id)).toList();
+        CompletableFuture<List<StudentEntity>> future = CompletableFuture.supplyAsync(this::getStudents);
 
-        if (students.size() != 1) {
-            return null;
-        } else {
-            return students.get(0);
-        }
+        List<StudentEntity> students = future.get().stream().filter(studentEntity -> Objects.equals(studentEntity.getId(), id)).toList();
+
+        return students.size() == 1 ? students.get(0) : null;
+    }
+
+    public List<StudentEntity> getStudents() {
+
+        return studentRepo.findAll();
+    }
+
+    public void updateStudent(final Long id) throws ExecutionException, InterruptedException {
+
+//        StudentEntity student = getStudent(id);
     }
 }
