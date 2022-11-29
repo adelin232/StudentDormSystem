2b657463b65a33d81844252391daa5baa33127f3
adelin232 <narcis.adelin.miulet@gmail.com>
2022-11-29 11:32:35 +0200

    Add when() and then() for MockBean of service

--- src/test/java/studentsdorm/platform/StudentTestIT.java
+++ src/test/java/studentsdorm/platform/StudentTestIT.java
@@ -2,14 +2,19 @@ package studentsdorm.platform;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import studentsdorm.platform.Student.Student;
 import studentsdorm.platform.Student.StudentController;
 import studentsdorm.platform.Student.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +30,10 @@ public class StudentTestIT {
     @Test
     @DisplayName("Test Controller methods")
     public void testControllerMethods() throws Exception {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Adelin", 239L, "+40747553042", "narcis.adelin.miulet@gmail.com", 8.56, false));
+
+        Mockito.when(studentService.getStudents()).thenReturn(students);
         mvc.perform(MockMvcRequestBuilders
                         .get("/students")
                         .accept(MediaType.APPLICATION_JSON))
