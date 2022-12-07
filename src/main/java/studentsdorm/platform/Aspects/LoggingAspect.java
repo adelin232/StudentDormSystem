ba71dadb757be01e74e965b73d7c7624692477ac
adelin232 <narcis.adelin.miulet@gmail.com>
2022-12-08 00:42:56 +0200

    Add AOP and AspectJ dependencies

--- src/main/java/studentsdorm/platform/Aspects/LoggingAspect.java
+++ src/main/java/studentsdorm/platform/Aspects/LoggingAspect.java
@@ -10,18 +10,9 @@ import org.aspectj.lang.annotation.Pointcut;
 @Slf4j
 public class LoggingAspect {
 
-    @Around("@within(studentsdorm.platform.Student.*)")
-    public Object log (ProceedingJoinPoint thisJoinPoint) throws Throwable {
-//        log.info("CALL METHOD " + methodName + " with args " + methodArgs);
-//        Object result = thisJoinPoint.proceed();
-//        log.info("METHOD " + methodName + " returns " + result);
-//
-//        return result;
-        return null;
-    }
+    @Around("execution(* studentsdorm.platform.Student.StudentService.createStudent(..))")
+    public void logAroundCreateStudent(ProceedingJoinPoint joinPoint) {
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    public void getMapping() {
     }
 }
 
