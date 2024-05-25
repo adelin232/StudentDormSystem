135c8f130557b00a495cca00d30c9fb7b03fd6df
adelin232 <narcis.adelin.miulet@gmail.com>
2024-05-26 01:36:21 +0300

    Change admin

--- src/main/java/studentsdorm/system/Admin/AdminService.java
+++ src/main/java/studentsdorm/system/Admin/AdminService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 @Service
 public class AdminService {
-    private final Set<String> adminUserIds = Set.of("CeUSCXcT3bZmSNZ0LXdu15HKXvw2");
+    private final Set<String> adminUserIds = Set.of("RgLtS2JWdiTDzo0FuZv1ZldCywL2");
 
     public boolean isAdmin(String userId) {
         return adminUserIds.contains(userId);
